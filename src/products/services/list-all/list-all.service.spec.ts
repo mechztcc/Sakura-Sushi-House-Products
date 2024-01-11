1d2231a5aa9f725ebd641879750c719f61a31e67
mechztcc <dojathan@gmail.com>
2024-01-11 08:30:15 -0500

    feat: added unit test to list all products

--- src/products/services/list-all/list-all.service.spec.ts
+++ src/products/services/list-all/list-all.service.spec.ts
@@ -1,18 +1,62 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ListAllService } from './list-all.service';
+import { PrismaService } from '../../../shared/prisma/services/prisma.service';
+import { Product } from '@prisma/client';
 
 describe('ListAllService', () => {
   let service: ListAllService;
+  let prismaServiceMock: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ListAllService],
+      providers: [
+        ListAllService,
+        {
+          provide: PrismaService,
+          useValue: {
+            product: {
+              findMany: jest.fn(),
+            },
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<ListAllService>(ListAllService);
+    prismaServiceMock = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should list all products', async () => {
+    const mockProducts: Product[] = [
+      {
+        id: 1,
+        name: 'Product 1',
+        description: 'Description 1',
+        price: '19.99',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 2,
+        name: 'Product 2',
+        description: 'Description 2',
+        price: '29.99',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ];
+
+    (prismaServiceMock.product.findMany as jest.Mock).mockResolvedValue(
+      mockProducts,
+    );
+
+    const result = await service.execute();
+
+    expect(prismaServiceMock.product.findMany).toHaveBeenCalled();
+    expect(result).toEqual(mockProducts);
   });
 });
--- src/products/services/list-all/list-all.service.ts
+++ src/products/services/list-all/list-all.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/shared/prisma/services/prisma.service';
+import { PrismaService } from '../../../shared/prisma/services/prisma.service';
 
 @Injectable()
 export class ListAllService {
